4954c643166ed2cab4c083d19dbc98d0f4d6e09c
DaRKkem <damross446@gmail.com>
2026-01-05 10:41:24 +0100

    commit

---
+++ av.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all arguments without using ac
+ * @ac: argument count (unused)
+ * @av: argument vector (NULL-terminated array)
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int i = 0;
+
+	(void)ac; /* explicitly mark ac as unused */
+
+	/* iterate through av until we hit NULL */
+	while (av[i] != NULL)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+
+	return (0);
+}
