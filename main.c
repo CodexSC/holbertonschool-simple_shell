138f795ca023bca46ccc1bcdb88fe58b7974a604
DaRKkem <damross446@gmail.com>
2026-01-06 17:38:35 +0100

    comment_added_to_main.c

--- main.c
+++ main.c
@@ -15,20 +15,16 @@ int main(void)
 	{
 		printf("$>");
 		fflush(stdout);
-
 		/* if CTRL+D pressed OR exit written, program exits */
 		if (getline(&line, &len, stdin) == -1)
 			break;
 		if (strcmp(line, "exit\n") == 0)
 			break;
-
 		/* change the last character of 'line' (\n) by the terminator '\0' */
 		line[strcspn(line, "\n")] = '\0';
-
 		/* if ENTER pressed but line empty, restart loop */
 		if (*line == '\0')
 			continue;
-
 		/* creates a child process, if a parent exists then it becomes the child */
 		pid = fork();
 		if (pid == 0)
