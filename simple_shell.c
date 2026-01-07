bb04d5c8903622a59662d5b353db35c3f7a84fd1
DaRKkem <damross446@gmail.com>
2026-01-07 15:37:35 +0100

    valid_betty

--- simple_shell.c
+++ simple_shell.c
@@ -9,36 +9,24 @@
  */
 int main(int argc, char *argv[])
 {
-	char *line = NULL;
+	char *line = NULL, **args;
 	size_t len = 0;
-	int is_interactive = isatty(STDIN_FILENO);
-	int line_num = 1;
-	char **args;
-	int i;
+	int is_interactive = isatty(STDIN_FILENO), line_num = 1, i;
 
 	(void)argc;
-
 	while (1)
 	{
 		if (is_interactive)
 			printf("($) ");
 		fflush(stdout);
-
 		if (getline(&line, &len, stdin) == -1)
-		{
 			if (is_interactive)
 				printf("\n");
 			break;
-		}
-
 		line[strcspn(line, "\n")] = '\0';
-
 		if (*line == '\0')
-		{
 			line_num++;
 			continue;
-		}
-
 		args = split_line(line);
 		if (args && args[0])
 		{
@@ -52,18 +40,13 @@ int main(int argc, char *argv[])
 			}
 			execute_cmd(args, argv[0], line_num);
 		}
-
 		if (args)
 		{
 			i = 0;
 			while (args[i])
 				free(args[i++]);
 			free(args);
-		}
-
-		line_num++;
-	}
-
-	free(line);
+		} line_num++;
+	} free(line);
 	return (0);
 }
