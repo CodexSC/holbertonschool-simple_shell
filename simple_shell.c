5bfbd6610e5d75420066202f43f3db79aadcd0b7
DaRKkem <damross446@gmail.com>
2026-01-07 12:11:11 +0100

    test_3

--- simple_shell.c
+++ simple_shell.c
@@ -1,77 +1,23 @@
 #include "shell.h"
+#include <unistd.h>
+#include <stdio.h>
 
-/**
- * trim_spaces - remove leading and trailing spaces/tabs
- * @line: input string
- *
- * Return: pointer to trimmed string
- */
-char *trim_spaces(char *line)
-{
-	while (*line == ' ' || *line == '\t')
-		line++;
-
-	if (*line == '\0')
-		return (line);
-
-	while (line[strlen(line) - 1] == ' ' ||
-	       line[strlen(line) - 1] == '\t')
-		line[strlen(line) - 1] = '\0';
-
-	return (line);
-}
-
-/**
- * execute_cmd - fork and execute command
- * @cmd: command to execute
- * @argv0: program name
- * @line_num: line number
- */
-void execute_cmd(char *cmd, char *argv0, int line_num)
-{
-	pid_t pid;
-	char *argv[2];
-
-	argv[0] = cmd;
-	argv[1] = NULL;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		return;
-	}
-
-	if (pid == 0)
-	{
-		execve(cmd, argv, environ);
-
-		fprintf(stderr, "%s: %d: %s: not found\n",
-		        argv0, line_num, cmd);
-		exit(127);
-	}
-	else
-	{
-		wait(NULL);
-	}
-}
-
-/**
- * main - simple shell
- */
 int main(int argc, char *argv[])
 {
 	char *line = NULL;
 	size_t len = 0;
-	int line_num = 1;
-	int interactive = isatty(STDIN_FILENO);
-	char *cmd;
+	pid_t pid;
+	int interactive;
 
 	(void)argc;
+	(void)argv;
+
+	interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		printf("$ ");
+		if (interactive)
+			printf("$ ");
 		fflush(stdout);
 
 		if (getline(&line, &len, stdin) == -1)
@@ -83,19 +29,23 @@ int main(int argc, char *argv[])
 
 		line[strcspn(line, "\n")] = '\0';
 
-		cmd = trim_spaces(line);
+		if (*line == '\0')
+			continue;
 
-		if (strcmp(cmd, "exit") == 0)
+		if (strcmp(line, "exit") == 0)
 			break;
 
-		if (*cmd == '\0')
+		pid = fork();
+		if (pid == 0)
 		{
-			line_num++;
-			continue;
-		}
+			char *args[] = {line, NULL};
 
-		execute_cmd(cmd, argv[0], line_num);
-		line_num++;
+			execve(line, args, environ);
+			perror(argv[0]);
+			exit(127);
+		}
+		else
+			wait(NULL);
 	}
 
 	free(line);
