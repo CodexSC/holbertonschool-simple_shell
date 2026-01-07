c2f8a8068538f53b610382d6115f9109d9b218b3
DaRKkem <damross446@gmail.com>
2026-01-07 12:07:53 +0100

    merge_codes

--- simple_shell.c
+++ simple_shell.c
@@ -1,19 +1,38 @@
 #include "shell.h"
-#include <unistd.h>
-#include <stdio.h>
 
 /**
- * execute_cmd - executes a command using fork and execve
- * @line: the command to execute
+ * trim_spaces - remove leading and trailing spaces/tabs
+ * @line: input string
  *
- * Return: void
+ * Return: pointer to trimmed string
  */
-void execute_cmd(char *line)
+char *trim_spaces(char *line)
+{
+	while (*line == ' ' || *line == '\t')
+		line++;
+
+	if (*line == '\0')
+		return (line);
+
+	while (line[strlen(line) - 1] == ' ' ||
+	       line[strlen(line) - 1] == '\t')
+		line[strlen(line) - 1] = '\0';
+
+	return (line);
+}
+
+/**
+ * execute_cmd - fork and execute command
+ * @cmd: command to execute
+ * @argv0: program name
+ * @line_num: line number
+ */
+void execute_cmd(char *cmd, char *argv0, int line_num)
 {
 	pid_t pid;
 	char *argv[2];
 
-	argv[0] = line;
+	argv[0] = cmd;
 	argv[1] = NULL;
 
 	pid = fork();
@@ -22,10 +41,13 @@ void execute_cmd(char *line)
 		perror("fork");
 		return;
 	}
+
 	if (pid == 0)
 	{
-		execve(line, argv, environ);
-		perror(line);
+		execve(cmd, argv, environ);
+
+		fprintf(stderr, "%s: %d: %s: not found\n",
+		        argv0, line_num, cmd);
 		exit(127);
 	}
 	else
@@ -35,47 +57,45 @@ void execute_cmd(char *line)
 }
 
 /**
- * main - entry point for the simple shell
- * Description: Creates an infinite loop that displays a prompt, reads user
- * commands, and executes them using fork and execve. Handles EOF (Ctrl+D)
- * and exit command gracefully. Only shows prompt in interactive mode.
- *
- * Return: Always 0
+ * main - simple shell
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	char *line = NULL;
 	size_t len = 0;
-	int is_interactive = isatty(STDIN_FILENO);
+	int line_num = 1;
+	int interactive = isatty(STDIN_FILENO);
+	char *cmd;
+
+	(void)argc;
 
 	while (1)
 	{
-		if (is_interactive)
-			printf("$ ");
+		printf("$ ");
 		fflush(stdout);
 
 		if (getline(&line, &len, stdin) == -1)
 		{
-			if (is_interactive)
+			if (interactive)
 				printf("\n");
 			break;
 		}
 
 		line[strcspn(line, "\n")] = '\0';
 
-		while (*line && (*line == ' ' || *line == '\t'))
-			line++;
+		cmd = trim_spaces(line);
 
-		while (*line && (line[strlen(line) - 1] == ' ' || line[strlen(line) - 1] == '\t'))
-			line[strlen(line) - 1] = '\0';
-
-		if (strcmp(line, "exit") == 0)
+		if (strcmp(cmd, "exit") == 0)
 			break;
 
-		if (*line == '\0')
+		if (*cmd == '\0')
+		{
+			line_num++;
 			continue;
+		}
 
-		execute_cmd(line);
+		execute_cmd(cmd, argv[0], line_num);
+		line_num++;
 	}
 
 	free(line);
