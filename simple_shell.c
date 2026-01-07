439d6720372f9856328b858d438434c1f0c86d1b
DaRKkem <damross446@gmail.com>
2026-01-07 12:12:26 +0100

    test_4

--- simple_shell.c
+++ simple_shell.c
@@ -2,50 +2,80 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+/**
+ * execute_cmd - executes a command using fork and execve
+ * @line: the command to execute
+ *
+ * Return: void
+ */
+void execute_cmd(char *line)
+{
+	pid_t pid;
+	char *argv[2];
+
+	argv[0] = line;
+	argv[1] = NULL;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+	if (pid == 0)
+	{
+		execve(line, argv, environ);
+		perror(line);
+		exit(127);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
+
+/**
+ * main - entry point for the simple shell
+ * Description: Creates an infinite loop that displays a prompt, reads user
+ * commands, and executes them using fork and execve. Handles EOF (Ctrl+D)
+ * and exit command gracefully. Only shows prompt in interactive mode.
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
-	pid_t pid;
-	int interactive;
-
-	(void)argc;
-	(void)argv;
-
-	interactive = isatty(STDIN_FILENO);
+	int is_interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		if (interactive)
-			printf("$ ");
+		if (is_interactive)
+			printf("($) ");
 		fflush(stdout);
 
 		if (getline(&line, &len, stdin) == -1)
 		{
-			if (interactive)
+			if (is_interactive)
 				printf("\n");
 			break;
 		}
 
 		line[strcspn(line, "\n")] = '\0';
 
-		if (*line == '\0')
-			continue;
+		while (*line && (*line == ' ' || *line == '\t'))
+			line++;
+
+		while (*line && (line[strlen(line) - 1] == ' ' || line[strlen(line) - 1] == '\t'))
+			line[strlen(line) - 1] = '\0';
 
 		if (strcmp(line, "exit") == 0)
 			break;
 
-		pid = fork();
-		if (pid == 0)
-		{
-			char *args[] = {line, NULL};
+		if (*line == '\0')
+			continue;
 
-			execve(line, args, environ);
-			perror(argv[0]);
-			exit(127);
-		}
-		else
-			wait(NULL);
+		execute_cmd(line);
 	}
 
 	free(line);
